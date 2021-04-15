15e1ff77c0da571e4a5a18271ba69f9b365bd2d1
Andrey-Gurevich <81981069+Andrey-Gurevich@users.noreply.github.com>
2021-04-15 21:59:28 +0300

    Add files via upload

--- Lab_4_task_1.py
+++ Lab_4_task_1.py
@@ -1,11 +1,11 @@
-def fibonacci_generator():
+def tribonacci_generator():
     a, b, c = 0, 0, 1
     while True:
         yield a
         a, b, c = b, c, a + b + c
 
 
-fib = fibonacci_generator()
+fib = tribonacci_generator()
 n = 1
 for i in range(35):
     print(str(n) + " число: ", next(fib))
