cc5000d316f907696e749e616f76c277a4980d5b
Andrey-Gurevich <81981069+Andrey-Gurevich@users.noreply.github.com>
2021-04-16 20:15:53 +0300

    Add files via upload

---
+++ Lab_4_task_2.py
@@ -0,0 +1,13 @@
+"""
+Скопировать из файла F1 в файл F2 строки, начиная с четвертой по порядку.
+Подсчитать количество символов в последнем слове F2.
+"""
+with open("F1.txt", "r") as file:
+    s = file.readlines()
+with open("F2.txt", "w") as file:
+    for i in range(3, len(s)):
+        file.write(s[i])
+with open("F2.txt", "r") as file:
+    last = file.readlines()[-1]
+    s = last.split()
+    print("Количество символов в последнем слове " + str(len(s[len(s)-1])))
